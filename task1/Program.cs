bb90244a25dc9d81ddb66136d776bf5dac9f46c1
maria <antonova.maria.a@gmail.com>
2023-06-15 20:11:06 +0300

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input a three-digits number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1000 & num > 99) {
+    num = num/10;
+    num = num %10;
+    Console.WriteLine(num);
+}
+else {
+    Console.WriteLine("Incorrect input");
+}
