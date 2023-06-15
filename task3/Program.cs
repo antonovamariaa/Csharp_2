911d4456916054aa17fd98fb25e39a3640b61ac8
maria <antonova.maria.a@gmail.com>
2023-06-15 20:32:13 +0300

    task3 done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input day number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >0 & num<8) {
+    if (num>0 & num<6) {
+        Console.WriteLine("no, it is a weekday");
+    }
+    else {
+        Console.WriteLine("yes, it is a weekend day");
+    }
+}
+else {
+    Console.WriteLine("Incorrect input");
+}
